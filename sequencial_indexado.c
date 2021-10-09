ecbdf4d116daf21f2ba214a0adc2a8bb530de6cf
Pedro Lucas Damasceno <rseiken@hotmail.com>
2021-10-09 18:05:30 -0300

    Avançando sequencial_indexado.c

--- sequencial_indexado.c
+++ sequencial_indexado.c
@@ -2,13 +2,35 @@
 
 int sequencial_Indexado(FILE *arquivo_Binario, int n_Metodo, int n_Registros, int n_Situacao, int n_Chave, char argv[5])
 {
-    int itens_Pagina = defineItensPagina(n_Registros);
+    int itens_Pagina = define_itensPagina(n_Registros);
     int tam_Tabela = (n_Registros / itens_Pagina) + 1;
     int tabela_Indice[tam_Tabela];
-    int indice_Pagina = buscarPagina(tabela_Indice, tam_Tabela, n_Situacao, n_Chave);
+    int indice_Pagina = buscarIndice(tabela_Indice, tam_Tabela, n_Situacao, n_Chave);
+
+    if (indice_Pagina == -1) //Caso toda a tabela de índice tenha sido percorrida e a chave seja maior que o último item.
+        return 0;
+
+    //Leitura da página referente ao indice encontrado na tabela.
+    Registro aux[itens_Pagina];
+    long int desloc_Ponteiro = indice_Pagina * sizeof(Registro);
+    fseek(arquivo_Binario, desloc_Ponteiro, SEEK_SET);
+    fread(&aux, sizeof(Registro), itens_Pagina, arquivo_Binario);
+
+    //Pesquisa sequencial na página lida.
+    for (int i = 0; i < itens_Pagina; i++)
+    {
+        if (aux[i].chave == n_Chave)
+        {
+            print_f("Registro encontrado!\n");
+            imprimir_Registro(aux[i]);
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
-int buscarPagina(int *tabela_Indice, int tam_Tabela, int n_Situacao, int n_Chave)
+int buscar_Indice(int *tabela_Indice, int tam_Tabela, int n_Situacao, int n_Chave)
 {
     int aux;
 
@@ -16,28 +38,34 @@ int buscarPagina(int *tabela_Indice, int tam_Tabela, int n_Situacao, int n_Chave
     {
     case 1:
         for (aux = 0; aux < tam_Tabela; aux++)
+        {
+            if (tabela_Indice[aux] == n_Chave)
+                return tabela_Indice[aux];
             if (tabela_Indice[aux] > n_Chave)
-                return (aux - 1);
-        return (aux - 1);
+                return tabela_Indice[aux - 1];
+        }
     case 2:
-        for (aux = tam_Tabela - 1; aux > -1; aux--)
+        for (aux = 0; aux < tam_Tabela; aux++)
+        {
+            if (tabela_Indice[aux] == n_Chave)
+                return tabela_Indice[aux];
             if (tabela_Indice[aux] < n_Chave)
-                return (aux + 1);
-        return (aux + 1);
+                return tabela_Indice[aux - 1];
+        }
     }
 
     return -1;
 }
 
-void montarTabela(FILE *arquivo, int *tabela_Indice, int itens_Pagina)
+void montar_Tabela(FILE *arquivo_Binario, int *tabela_Indice, int itens_Pagina)
 {
     Registro aux[itens_Pagina];
 
-    for (int i = 0; fread(&aux, sizeof(Registro), itens_Pagina, arquivo); i++)
+    for (int i = 0; fread(&aux, sizeof(Registro), itens_Pagina, arquivo_Binario); i++)
         tabela_Indice[i] = aux[0].chave;
 }
 
-int defineItensPagina(int n_Registros)
+int define_itensPagina(int n_Registros)
 {
     switch (n_Registros)
     {
