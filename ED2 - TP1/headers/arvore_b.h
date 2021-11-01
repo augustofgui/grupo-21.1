e93404ceaa74e3a384c9f4f12f701977641b2495
Caio Monteiro <cainhomo@hotmail.com>
2021-11-01 15:55:32 -0300

    avançando arvore b

--- ED2 - TP1/headers/arvore_b.h
+++ ED2 - TP1/headers/arvore_b.h
@@ -3,16 +3,23 @@
 #define MM 4
 #define M 2
 
+int cont = -1; int count = 0;
+
 typedef struct TipoPagina *TipoApontador;
-typedef struct TipoPagina
-{
+typedef struct TipoPagina {
+    int pageNum;
     short n;
     Registro r[MM];
     TipoApontador p[MM + 1];
 } TipoPagina;
 
 void Inicializa(TipoApontador Arvore);
+int Pesquisa(FILE *arquivo_binario, Registro *x, TipoApontador Ap);
+int PesquisanoArquivo(FILE *arquivo_binario, Registro Reg, TipoApontador pagina);
 void Imprime(TipoApontador arvore);
 void InsereNaPagina(TipoApontador Ap, Registro Reg, TipoApontador ApDir);
 void Ins(Registro Reg, TipoApontador Ap, short *Cresceu, Registro *RegRetorno, TipoApontador *ApRetorno);
-void Insere(Registro Reg, TipoApontador *Ap);
+void salvar(FILE *arquivo_binario, TipoApontador pagina, Registro Reg[]);
+void saveAux(FILE *arquivo_binario, TipoApontador p, int Nivel);
+void Insere(FILE *arquivo_binario, Registro Reg, TipoApontador *Ap);
+int arvore_externa(FILE *arquivo_binario, int nro_metodo, int nro_registros, int nro_situacao, int nro_chave, char argv[5]);
--- ED2 - TP1/src/arvore_b.c
+++ ED2 - TP1/src/arvore_b.c
@@ -5,9 +5,10 @@ void Inicializa(TipoApontador Arvore)
     Arvore = NULL;
 }
 
-int Pesquisa(Registro *x, TipoApontador Ap)
-{
+int Pesquisa(FILE *arquivo_binario, Registro *x, TipoApontador Ap) {
     long i = 1;
+    int pes;
+    int achou;
     if (Ap == NULL)
         return 0;
 
@@ -15,17 +16,32 @@ int Pesquisa(Registro *x, TipoApontador Ap)
         i++;
     if (x->chave == Ap->r[i - 1].chave)
     {
-        *x = Ap->r[i - 1];
-        return 1;
+        achou = PesquisanoArquivo(arquivo_binario,Ap->r[i-1],Ap);
+        return achou;
     }
     if (x->chave < Ap->r[i - 1].chave)
-        Pesquisa(x, Ap->p[i - 1]);
+        pes = Pesquisa(arquivo_binario, x, Ap->p[i - 1]);
     else
-        Pesquisa(x, Ap->p[i]);
+        pes = Pesquisa(arquivo_binario, x, Ap->p[i]);
+    return 0;    
 }
 
-void Imprime(TipoApontador arvore)
-{
+int PesquisanoArquivo(FILE *arquivo_binario, Registro Reg, TipoApontador pagina) {
+    Registro reg[2*M];
+    int i;
+    FILE *arq = fopen(arquivo_binario,"rb");
+    if (arq == NULL)
+    exit(1);
+    fseek(arq, pagina->pageNum*(2*M*sizeof(Registro)), SEEK_SET);
+    fread(reg, (2*M*sizeof(Registro)),1,arq);
+    fclose(arq);
+    for(i = 0; i < 2*M; i++) {
+        if (Reg.chave == reg[i].chave)
+            return 1;
+    }        
+}    
+
+void Imprime(TipoApontador arvore) {
     int i = 0;
     if (arvore == NULL)
         return;
@@ -83,12 +99,13 @@ void Ins(Registro Reg, TipoApontador Ap, short *Cresceu, Registro *RegRetorno, T
         return;
     }
     if (Reg.chave < Ap->r[i - 1].chave)
-        i--;
-    Ins(Reg, Ap->p[i], Cresceu, RegRetorno, ApRetorno);
-    if (Cresceu == 0)
+        Ins(Reg, Ap->p[i-1], Cresceu, RegRetorno, ApRetorno);
+    else
+        Ins(Reg, Ap->p[i], Cresceu, RegRetorno, ApRetorno);    
+    if (!*Cresceu)
         return;
     if (Ap->n < MM)
-    { // Página tem espaço
+    { // Verificando se tem espaço na página
         InsereNaPagina(Ap, *RegRetorno, *ApRetorno);
         *Cresceu = 0;
         return;
@@ -97,6 +114,8 @@ void Ins(Registro Reg, TipoApontador Ap, short *Cresceu, Registro *RegRetorno, T
     ApTemp = (TipoApontador)malloc(sizeof(TipoPagina));
     ApTemp->n = 0;
     ApTemp->p[0] = NULL;
+    cont++;
+    ApTemp->pageNum = cont;
     if (i < M + 1)
     {
         InsereNaPagina(ApTemp, Ap->r[MM - 1], Ap->p[MM]);
@@ -113,19 +132,74 @@ void Ins(Registro Reg, TipoApontador Ap, short *Cresceu, Registro *RegRetorno, T
     *ApRetorno = ApTemp;
 }
 
-void Insere(Registro Reg, TipoApontador *Ap)
-{
+void salvar(FILE *arquivo_binario, TipoApontador pagina, Registro Reg[]) {
+
+    FILE* arq;
+    if (!file_exists(arquivo_binario)){
+        arq = fopen(arquivo_binario,"wb");
+        if (arq == NULL)
+            exit(1);
+        fseek(arq, pagina->pageNum*(MM*sizeof(Registro)), SEEK_SET);
+        fwrite(Reg,(MM*sizeof(Registro)),1,arq);
+        fclose(arq);
+    }
+
+    else{
+        arq = fopen(arquivo_binario,"r+b");
+        if (arq == NULL)
+            exit(1);
+        fseek(arq, pagina->pageNum*(MM*sizeof(Registro)), SEEK_SET);
+        fwrite(Reg, (MM*sizeof(Registro)),1,arq);
+        fclose(arq);
+        }
+    }
+
+void saveAux(FILE *arquivo_binario, TipoApontador p, int Nivel) {
+  int i,j;
+
+  if (p == NULL)
+    return;
+  for (i = 0; i < p->n; i++)
+    salvar(arquivo_binario, p, p->r);
+  for (j = 0; j <= p->n; j++)
+    saveAux(arquivo_binario, p->p[j], Nivel + 1);
+}
+
+void Insere(FILE *arquivo_binario, Registro Reg, TipoApontador *Ap) {
     short Cresceu;
     Registro RegRetorno;
     TipoPagina *ApRetorno, *ApTemp;
     Ins(Reg, *Ap, &Cresceu, &RegRetorno, &ApRetorno);
-    if (Cresceu == 1)
+    if (Cresceu)
     { // Caso a árvore tenha crescido
         ApTemp = (TipoPagina *)malloc(sizeof(TipoPagina));
         ApTemp->n = 1;
         ApTemp->r[0] = RegRetorno;
         ApTemp->p[1] = ApRetorno;
+        cont++;
+        ApTemp->pageNum = cont;
         ApTemp->p[0] = *Ap;
         *Ap = ApTemp;
     }
+    // Salvar no arquivo
+    saveAux(arquivo_binario,*Ap, MM);
+}
+
+int arvore_externa(FILE *arquivo_binario, int nro_metodo, int nro_registros, int nro_situacao, int nro_chave, char argv[5]) {
+    TipoApontador *arv;
+    int pesquisa;
+    Registro reg;
+    arv=(TipoApontador*) malloc(sizeof(TipoApontador));
+    Inicializa(arv);
+    pesquisa = Pesquisa(arquivo_binario, nro_chave, arv);
+    if (pesquisa == 1) {
+        Insere(arquivo_binario, reg, arv);
+        count++;
+        if (nro_chave <= 0) {
+            count--;
+        }
+        return 1;
+    }
+    else 
+        return 0;
 }
