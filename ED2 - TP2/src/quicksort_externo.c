793bfa28e9bb9fa6e43f6115ea2b8000c38f5b17
Pedro Lucas Damasceno <rseiken@hotmail.com>
2021-12-02 11:01:11 -0300

    Avançando quicksort_externo

--- ED2 - TP2/src/quicksort_externo.c
+++ ED2 - TP2/src/quicksort_externo.c
@@ -3,15 +3,13 @@
 
 void quicksort_externo_main(char argv[], int nro_quantidade)
 {
-    FILE *ArqLi, *ArqEi, *ArqLEs, *ArqTXT, *ArqBinario;
-    ArqTXT = abrir_arquivo(argv, "r");
-    ArqBinario = converter_para_binario(ArqTXT, "arquivo_binario.bin");
+    FILE *ArqLi, *ArqEi, *ArqLEs;
     ArqLi = abrir_arquivo("arquivo_binario.bin", "r+b");
     ArqEi = abrir_arquivo("arquivo_binario.bin", "r+b");
     ArqLEs = abrir_arquivo("arquivo_binario.bin", "r+b");
     quicksort_externo(&ArqLi, &ArqEi, &ArqLEs, 1, nro_quantidade);
     rewind(ArqLi);
-    converter_para_txt(ArqBinario, "arquivo_texto.txt");
+    converter_para_txt(ArqLi, "arquivo_texto.txt");
 }
 
 void quicksort_externo(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, int Esq, int Dir)
@@ -38,14 +36,6 @@ void quicksort_externo(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, int Esq, int D
     }
 }
 
-void FAVazia(TipoArea* Area){
-    for (int i = 0; i < TAM_AREA; i++)
-        Area->array[i].nota = 99999.99;
-    Area->nro_cels_ocupadas = 0;
-}
-
-
-
 void Particao(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, TipoArea Area, int Esq, int Dir, int *i, int *j)
 {
     int Ls = Dir, Es = Dir, Li = Esq, Ei = Esq, NRArea = 0, Linf = INT_MIN, Lsup = INT_MAX;
@@ -68,6 +58,8 @@ void Particao(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, TipoArea Area, int Esq,
             InserirArea(&Area, &UltLido, &NRArea);
             continue;
         }
+
+        // ------ OK ATÉ AQUI ------ //
 
         if (Ls == Es)
             LeSup(ArqLEs, &UltLido, &Ls, &OndeLer);
@@ -114,14 +106,16 @@ void Particao(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, TipoArea Area, int Esq,
     }
 }
 
+// ----- OK ----- //
 void LeSup(FILE **ArqLEs, Registro *UltLido, int *Ls, short *OndeLer)
 {
-    fseek(*ArqLEs, (*Ls - 1) * sizeof(Registro), SEEK_SET);
+    fseek(*ArqLEs, ((*Ls - 1) * sizeof(Registro)), SEEK_SET);
     fread(UltLido, sizeof(Registro), 1, *ArqLEs);
     (*Ls)--;
     *OndeLer = 0;
 }
 
+// ----- OK ----- //
 void LeInf(FILE **ArqLi, Registro *UltLido, int *Li, short *OndeLer)
 {
     fread(UltLido, sizeof(Registro), 1, *ArqLi);
@@ -129,51 +123,68 @@ void LeInf(FILE **ArqLi, Registro *UltLido, int *Li, short *OndeLer)
     *OndeLer = 1;
 }
 
+// ----- OK ----- //
 void InserirArea(TipoArea *Area, Registro *UltLido, int *NRArea)
 {
     InsereItem(UltLido, Area);
     *NRArea = Area->nro_cels_ocupadas;
 }
 
+// ----- OK ----- //
 void InsereItem(Registro *UltLido, TipoArea *Area){
     Area->array[Area->nro_cels_ocupadas] = *UltLido;
     Area->nro_cels_ocupadas++;
     merge_sort(Area->array, 0, TAM_AREA - 1);
 }
 
+// ----- OK ----- //
 void RetiraUltimo(TipoArea *Area, Registro *R){
     *R = Area->array[Area->nro_cels_ocupadas - 1];
-    Area->array[Area->nro_cels_ocupadas].nota = 99999.99;
+    Area->array[Area->nro_cels_ocupadas - 1].nota = 99999.99;
+    Area->nro_cels_ocupadas--;
     merge_sort(Area->array, 0, TAM_AREA - 1);
 }
 
+// ----- OK ----- //
 void RetiraPrimeiro(TipoArea *Area, Registro *R){
     *R = Area->array[0];
     Area->array[0].nota = 99999.99;
+    Area->nro_cels_ocupadas--;
     merge_sort(Area->array, 0, TAM_AREA - 1);
 }
 
+// ----- OK ----- //
 void EscreveMax(FILE **ArqLEs, Registro R, int *Es)
 {
-    fseek(*ArqLEs, (*Es - 1) * sizeof(Registro), SEEK_SET);
+    fseek(*ArqLEs, ((*Es - 1) * sizeof(Registro)), SEEK_SET);
     fwrite(&R, sizeof(Registro), 1, *ArqLEs);
     (*Es)--;
 }
 
+// ----- OK ----- //
 void EscreveMin(FILE **ArqEi, Registro R, int *Ei)
 {
     fwrite(&R, sizeof(Registro), 1, *ArqEi);
     (*Ei)++;
 }
 
+// ----- OK ----- //
 void RetiraMax(TipoArea *Area, Registro *R, int *NRArea)
 {
     RetiraUltimo(Area, R);
     *NRArea = Area->nro_cels_ocupadas;
 }
 
+// ----- OK ----- //
 void RetiraMin(TipoArea *Area, Registro *R, int *NRArea)
-{where
+{
     RetiraPrimeiro(Area, R);
     *NRArea = Area->nro_cels_ocupadas;
+}
+
+// ----- OK ----- //
+void FAVazia(TipoArea* Area){
+    for (int i = 0; i < TAM_AREA; i++)
+        Area->array[i].nota = 99999.99;
+    Area->nro_cels_ocupadas = 0;
 }
