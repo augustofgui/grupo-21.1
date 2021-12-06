6a3dbe20472b7afae4a64d1c7187cd4082312ad2
pvdcmc <pvdcmc@yahoo.com.br>
2021-12-06 14:58:08 -0300

    intercalacao substituicao selecao mudancas

--- ED2 - TP2/headers/intercalacao_substituicao_selecao.h
+++ ED2 - TP2/headers/intercalacao_substituicao_selecao.h
@@ -6,10 +6,24 @@
 #include <stdlib.h>
 #include <limits.h> 
 
+#define MAXTAM 20
+
 void preencheVetor (Registro* vetor,FILE* fileEntrada);
 void converter_para_char (int num,char* c);
 void esvaziarVetor (Registro* vetor,int* n);
 void sub (FILE* nome_arquivo, Registro* vetor,int* n);
 void intercalacao_substituicao_selecao(char* nome_arquivo, bool imprimir_dados);
+
+////////////////////HEAP
+
+void Refaz (int Esq, int Dir, Registro *A);
+void RefazPeloIndice (int Esq, int Dir, Registro *A);
+void Constroi (Registro *A, int  *n);
+void ConstroiPeloIndice (Registro *A, int *n);
+Registro Min (Registro *A);
+Registro RetiraMin (Registro *A, int *n);
+void Aumenta (int i, double new, Registro *A);
+void Insere (Registro *x, Registro *A, int *n);
+void Imprime (Registro *A, int *n);
 
 #endif
--- ED2 - TP2/src/intercalacao_substituicao_selecao.c
+++ ED2 - TP2/src/intercalacao_substituicao_selecao.c
@@ -1,15 +1,18 @@
 #include "../headers/geral.h"
 #include "../headers/intercalacao_substituicao_selecao.h"
-#include "../headers/heap_min.h"
 
 #define TAM 10 // heap
 #define NUM_BLOCK 20 // numero de blocos
 
 int N = TAM;
 
+int comparacoes = 0;
+int leituras = 0;
+int escritas = 0;
+
 void preencheVetor(Registro* vetor, FILE* fileEntrada) {
     for(int i = 1; i <= TAM; i++) {
-        nro_iss_leituras++;
+         leituras++;
         fread(&vetor[i], sizeof(*vetor), 1, fileEntrada);
     }
     Constroi(vetor, &N);
@@ -36,7 +39,7 @@ void esvaziarVetor (Registro* vetor, int* n) {
         strcat(block, bin);
         fileSaida = fopen(block, "ab");
         strcpy(block, "Bloco");
-        nro_iss_escritas++;
+         escritas++;
         fwrite(&Min, sizeof(Min), 1, fileSaida);
         fflush(stdout);
         fclose(fileSaida);
@@ -60,28 +63,28 @@ void sub (FILE* nome_arquivo, Registro* vetor, int* n ) {
 
     FILE* fileSaida = fopen("Bloco1.bin", "ab");
     Mini = RetiraMin(vetor, n);
-    nro_iss_escritas++;
+     escritas++;
     fwrite(&Mini, sizeof(Mini), 1, fileSaida);
     fclose(fileSaida);
 
     while (fread(&Item, sizeof(Item), 1, nome_arquivo) == 1) {
-        nro_iss_leituras++;
-        nro_iss_comparacoes++;
+         leituras++;
+         comparacoes++;
         if(Item.nota < Mini.nota) {
-            nro_iss_comparacoes++;
+             comparacoes++;
             if(fita == NUM_BLOCK) {
                 fita = 1;
             }
             Item.f = fita + 1;
             Insere(&Item,vetor,n);
          }
-        nro_iss_comparacoes++;
+         comparacoes++;
         if(Item.nota >= Mini.nota) {
             Item.f = fita;
             Insere(&Item,vetor,n);
             
         }
-        nro_iss_comparacoes++;
+         comparacoes++;
         if(Min(vetor).f > fita) {
             fita++;
         }
@@ -93,7 +96,7 @@ void sub (FILE* nome_arquivo, Registro* vetor, int* n ) {
         strcat(block, txt);
         fileSaida = fopen(block, "ab");
         strcpy(block, "Bloco");
-        nro_iss_escritas++;
+         escritas++;
         fwrite(&Mini, sizeof(Mini), 1, fileSaida);
         fflush(stdout);
         fclose(fileSaida);
@@ -116,5 +119,145 @@ void intercalacao_substituicao_selecao(char* nome_arquivo, bool imprimir_dados) 
     t = clock() - t;
     double tempo_execucao = ((double)t) / CLOCKS_PER_SEC;
 
-    print_estatisticas(nro_iss_comparacoes, nro_iss_leituras, nro_iss_escritas, tempo_execucao);
+   if(imprimir_dados){}
+    print_estatisticas(comparacoes,  leituras,  escritas, tempo_execucao);
+}
+
+////////////////////
+////////////////////
+////////////////////HEAP
+////////////////////
+////////////////////
+
+void troca (Registro* A, int n) {
+    Registro aux;
+    for (int i = 1 ; i < n-1 ; i = (i*2) - 1) {
+         comparacoes+=2;
+        if(A[i].nota > A[i+1].nota && A[i].f == A[i+1].f) {
+        aux = A[i+1];
+        A[i+1] = A[i];
+        A[i] = aux;  
+        }
+    }
+}
+
+void Refaz (int Esq, int Dir, Registro *A) { 
+    int i = Esq;
+    int j;
+    Registro x;
+    j = (i * 2);
+    x = A[i];
+    while (j <= Dir) {
+         comparacoes++;
+        if (j < Dir) { 
+             comparacoes++;
+            if (A[j].nota > A[j+1].nota) {
+            j++;
+            }    
+        }
+         comparacoes++;
+        if (x.nota <= A[j].nota) { 
+            A[i] = x;
+        }
+        A[i] = A[j];
+        i = j; 
+        j = i * 2;
+    }
+} 
+
+void RefazPeloIndice (int Esq, int Dir, Registro *A) { 
+    int i = Esq;
+    int j;
+    Registro x;
+    j = (i * 2);
+    x = A[i];
+    while (j <= Dir) { 
+         comparacoes++;
+        if (j < Dir) { 
+             comparacoes++;
+            if (A[j].f > A[j+1].f) {
+                j++;
+            }       
+        }
+         comparacoes++;
+        if (x.f <= A[j].f) { 
+            A[i] = x;
+        }         
+        A[i] = A[j];
+        i = j; 
+        j = i * 2;
+    }
+} 
+
+void Constroi (Registro *A, int *n) {
+    int Esq;
+    Esq = *n / 2 + 1;
+    while (Esq > 1) {
+        Esq--;
+        Refaz(Esq, *n, A);
+    }
+} 
+
+void ConstroiPeloIndice (Registro *A, int *n) {
+    int Esq;
+    Esq = *n /2 + 1 ;
+
+    while (Esq > 1) { 
+        Esq--;
+        RefazPeloIndice(Esq, *n, A);
+    }
+} 
+
+Registro Min(Registro *A) {
+    return (A[1]); 
+}
+
+Registro RetiraMin (Registro *A, int *n) {
+    Registro Min;
+     comparacoes++;
+    if (*n < 1) 
+    printf("Erro!!\n");
+    else {
+        Min = A[1];  
+        A[1] = A[*n];  
+        (*n)--;
+        Refaz(1, *n, A);
+        RefazPeloIndice(1,*n,A);
+    }
+    Constroi(A,n);
+    ConstroiPeloIndice(A,n);
+    return Min;
+}
+
+void Aumenta (int i, double new, Registro *A) { 
+    Registro x;
+     comparacoes++;
+    if (new < A[i].nota) { 
+        printf("Erro!!\n");
+        return;
+    }
+    A[i].nota = new;
+    while (i > 1 && A[i / 2].nota > A[i].nota) { 
+        x = A[i / 2]; 
+        A[i / 2] = A[i];  
+        A[i] = x;
+        i /= 2;
+    }
+}
+
+void Insere (Registro *x, Registro *A, int *n) {
+    (*n)++;
+    A[*n] = *x;  
+    A[*n].nota = INT_MIN;
+    Aumenta(*n, x->nota, A);
+    Constroi(A,n);
+    ConstroiPeloIndice(A,n);
+}
+
+void Imprime (Registro *A, int *n) { 
+    int i;
+    for (i = 1; i <= *n; i++){
+        printf("%f ", A[i].nota);
+    }
+    printf("\n");
 }
