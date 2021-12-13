5733e09214938c3d355f3697854cde999b954e60
Pedro Lucas Damasceno <rseiken@hotmail.com>
2021-12-13 18:56:37 -0300

    Avançando funções e ordenação

--- POO - TP2/TP2POO.Imobiliaria/src/main/java/TP2POO/Imobiliaria/Imobiliaria.java
+++ POO - TP2/TP2POO.Imobiliaria/src/main/java/TP2POO/Imobiliaria/Imobiliaria.java
@@ -2,6 +2,8 @@ package TP2POO.Imobiliaria;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -41,29 +43,29 @@ public class Imobiliaria {
         }
     }
 
-    public static void ordenarCrescente(ArrayList<Imovel> imoveis){
+    public static void ordenarCrescente(ArrayList<Imovel> imoveis) {
         Collections.sort(imoveis);
     }
 
-    public static ArrayList<Imovel> buscarPorTipo(ArrayList<Imovel> imoveis){
+    public static ArrayList<Imovel> buscarPorTipo(ArrayList<Imovel> imoveis) {
         ArrayList<Imovel> resultado = new ArrayList<Imovel>();
         int tipo_imovel = 0; // - > placeholder
 
-        switch(tipo_imovel){
-            case 1 :
-                for (Imovel imovel : imoveis){
+        switch (tipo_imovel) {
+            case 1:
+                for (Imovel imovel : imoveis) {
                     if (imovel instanceof Casa)
                         resultado.add(imovel);
                 }
                 break;
-            case 2 :
-                for (Imovel imovel : imoveis){
+            case 2:
+                for (Imovel imovel : imoveis) {
                     if (imovel instanceof Apartamento)
                         resultado.add(imovel);
                 }
                 break;
-            case 3 :
-                for (Imovel imovel : imoveis){
+            case 3:
+                for (Imovel imovel : imoveis) {
                     if (imovel instanceof Chacara)
                         resultado.add(imovel);
                 }
@@ -73,12 +75,60 @@ public class Imobiliaria {
         return resultado;
     }
 
+    public static void salvarColecao(ArrayList<Imovel> imoveis) {
+        try {
+            FileWriter colecao = new FileWriter("colecao_imoveis.txt");
+            for (Imovel imovel : imoveis) {
+                if (imovel instanceof Casa)
+                    colecao.write(writeCasa(imovel) + "\n");
+                else if (imovel instanceof Apartamento)
+                    colecao.write(writeApartamento(imovel) + "\n");
+                else if (imovel instanceof Chacara)
+                    colecao.write(writeChacara(imovel) + "\n");
+            }
+            colecao.close();
+            System.out.println("Coleção salva em colecao_imoveis.txt!");
+        } catch (IOException e) {
+            System.out.println("ERRO : I/O.");
+            e.printStackTrace();
+        }
+    }
+
+    public static String writeCasa(Imovel imovel) {
+        return "casa;" + imovel.getValor() + ";" + imovel.getProprietario() + ";" + imovel.getRua() + ";"
+                + imovel.getBairro() + ";" + imovel.getCidade() + ";" + imovel.getNumero() + ";" + imovel.getQuartos()
+                + ";" + imovel.getBanheiros() + ";" + ((Casa) imovel).getAndares() + ";"
+                + ((Casa) imovel).isSala_jantar();
+    }
+
+    public static String writeApartamento(Imovel imovel) {
+        return "apartamento;" + imovel.getValor() + ";" + imovel.getProprietario() + ";" + imovel.getRua() + ";"
+                + imovel.getBairro() + ";" + imovel.getCidade() + ";" + imovel.getNumero() + ";" + imovel.getQuartos()
+                + ";" + imovel.getBanheiros() + ";" + ((Apartamento) imovel).getAndar() + ";"
+                + ((Apartamento) imovel).getTaxa_condominio() + ";" + ((Apartamento) imovel).isElevador() + ";"
+                + ((Apartamento) imovel).isSacada();
+    }
+
+    public static String writeChacara(Imovel imovel) {
+        return "chacara;" + imovel.getValor() + ";" + imovel.getProprietario() + ";" + imovel.getRua() + ";"
+                + imovel.getBairro() + ";" + imovel.getCidade() + ";" + imovel.getNumero() + ";" + imovel.getQuartos()
+                + ";" + imovel.getBanheiros() + ";" + ((Chacara) imovel).isSalao_festa() + ";"
+                + ((Chacara) imovel).isSalao_jogos() + ";" + ((Chacara) imovel).isCampo_futebol() + ";"
+                + ((Chacara) imovel).isChurrasqueira() + ";" + ((Chacara) imovel).isPiscina();
+    }
+
     public static ArrayList<Imovel> buscarProprietario(ArrayList<Imovel> imoveis) {
         ArrayList<Imovel> resultado = new ArrayList<Imovel>();
         for (Imovel imovel : imoveis) {
             if (imovel.getProprietario().equals("#placeholder#"))
                 resultado.add(imovel);
         }
+
+        /*
+         * É possível utilizar essa função para os critérios 2 e 7.
+         * Para o 7, basta imprimir o array 'resultado',
+         * Para o 2, basta verificar se 'resultado' é um array vazio.
+         */
 
         return resultado;
     }
